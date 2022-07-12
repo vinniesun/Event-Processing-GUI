484d4e9266681fd034f46c14cb0213a81853bef7
Vincent Sun <vincentpssun@gmail.com>
2022-07-12 14:18:38 +0800

    added guiprocess

---
+++ events/include/guiprocess.hpp
@@ -0,0 +1,86 @@
+//#pragma once
+#ifndef GUIPROCESS_H
+#define GUIPROCESS_H
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_stdlib.h"       // This header file is needed for using std::string with ImGui::InputText() instead of char*
+
+#include "process.hpp"
+
+static double multiplier = 1e6;
+
+class guiProcess{
+public:
+    guiProcess();
+    ~guiProcess();
+    Events preprocess_file();
+    void preprocess_initialisation();
+    void preprocess_run();
+    int run_display_reconstructed();
+    void live_process_initialisation();
+    void live_process_run();
+    void resetFunction();
+
+    bool gui_status;                     // Display the current status of the GUI
+    bool show_setting;                   // Display the processing setting window
+
+    int frame;                           // Frame determines which frame of the images should be displayed
+    int frame_debug;
+    int time_window;                     // The time window for how long (in us) events should be accumulated for to generate a frame
+    int height;                          // The height of the Event Camera
+    int width;                           // The width of the Event Camera
+    int ref_period;                      // The refractory period for Refractory Filtering
+    int nn_window;                       // The time window for Nearest Neighbourhood Filtering
+    int no_of_events;                    // The value determines number of events that will be processed
+    int event_no;                        // The current event number to process
+    int ktos;                            // The ktos parameter for updating TOS
+    int ttos;                            // The ttos parameter for updating TOS
+    int prev_time;                       // This variable keeps track of the timestamp of the previous event
+    int quant;                           // This is the quant variable for updating SAE
+
+    bool on_off_check;                   // This flag checks if On and Off events should be stored separately or together in a Time Surface
+    bool tos_check;                      // This flag checks if the events are to be stored in a TOS (when true) or a Time Surface (when false, the default)
+    bool start_processing;               // This flag indicates whether the event text file will be processed or not.
+    bool display_reconstructed;          // This flag indicates whether the reconstructed images should be displayed
+    bool display_live;                   // This flag indicates whether the events will be processed in a live manner.
+    bool display_processed;              // This flag indicates whether the event is displayed post-processed.
+    bool preprocessing;                  // This flag indicates if we need to preprocess the event txt file.
+    bool reset;                          // This flag indicates if the class variables are to be reset.
+    bool davis240;                       // This flag indicates that the input event file is generated with a DAVIS240
+    bool davis346;                       // This flag indicates that the input event file is generated iwth a DAVIS346
+    bool first_recon;                    // This flag checks if this is the first reconstructed frame to be displayed. If it is, then the OpenGL texture object will be updated
+    bool first_live;                     // This flag is currently unused, might delete later
+
+    std::string filename;                // The path to the event text file
+    std::string recon_path;              // The reconstructed images folder's path
+
+    Events process_events;               // The Events object used in post-processed display that holds all of the valid events
+    cv::Mat surface_area;                // This Mat object is the Time Surface that's updated per event. Can be either TOS or traditional Time Surface
+    std::string mode;                    // This string indicates what kind of Time Surface Update scheme that's used.
+
+    std::fstream event_file;                    // The fstream object for reading the files.
+    int line_no;                         // This keeps track of the number of lines read by the fstream object
+
+    int x_prev;                          // Used by NN filtering
+    int y_prev;                          // Used by NN filtering
+    int p_prev;                          // Used by NN filtering
+    std::vector<std::vector<int>> ref_mask;     // This is the 2D vector used for Refractory Filtering
+    std::vector<std::vector<int>> nn_mask;      // This is the 2D vector used for NN Filtering
+
+    GLuint texture;                             // This is the texture ID used for live processing and post-processing
+    GLuint texture_recon;                       // This is the texture ID used for displaying reconstructed images.
+    cv::Mat image;                              // This is the Mat object that holds the current image to be translated to OpenGL texture for display (live processing & post-processing)
+    cv::Mat image_recon;                        // This is the Mat object that holds the current reconstructed frame to be translated to OpenGL texture for display
+};
+
+#endif
---
+++ events/src/guiprocess.cpp
@@ -0,0 +1,303 @@
+#include "guiprocess.hpp"
+
+guiProcess::guiProcess() {
+    gui_status = true;                  // Display the current status of the GUI
+    show_setting = true;                // Display the processing setting window
+
+    frame = 1;                           // Frame determines which frame of the images should be displayed
+    frame_debug = 1;
+    time_window = 0;                     // The time window for how long (in us) events should be accumulated for to generate a frame
+    height = 0;                          // The height of the Event Camera
+    width = 0;                           // The width of the Event Camera
+    ref_period = 0;                      // The refractory period for Refractory Filtering
+    nn_window = 0;                       // The time window for Nearest Neighbourhood Filtering
+    no_of_events = -1;                   // The value determines number of events that will be processed
+    event_no = 0;                        // The current event number to process
+    ktos = 0;                            // The ktos parameter for updating TOS
+    ttos = 0;                            // The ttos parameter for updating TOS
+    prev_time = 0;                       // This variable keeps track of the timestamp of the previous event
+    quant = 0;                           // This is the quant variable for updating SAE
+
+    on_off_check = false;               // This flag checks if On and Off events should be stored separately or together in a Time Surface
+    tos_check = false;                  // This flag checks if the events are to be stored in a TOS (when true) or a Time Surface (when false, the default)
+    start_processing = false;           // This flag indicates whether the event text file will be processed or not.
+    display_reconstructed = false;      // This flag indicates whether the reconstructed images should be displayed
+    display_live = false;               // This flag indicates whether the events will be processed in a live manner.
+    display_processed = false;          // This flag indicates whether the event is displayed post-processed.
+    preprocessing = false;              // This flag indicates if we need to preprocess the event txt file.
+    reset = false;                      // This flag indicates if the class variables are to be reset.
+    davis240 = false;                   // This flag indicates that the input event file is generated with a DAVIS240
+    davis346 = false;                   // This flag indicates that the input event file is generated iwth a DAVIS346
+    first_recon = true;                 // This flag checks if this is the first reconstructed frame to be displayed. If it is, then the OpenGL texture object will be updated
+    first_live = true;                  // This flag is currently unused, might delete later
+
+    filename = "";               // The path to the event text file
+    recon_path = "";             // The reconstructed images folder's path
+
+    mode = "";                   // This string indicates what kind of Time Surface Update scheme that's used.
+
+    line_no = 0;                         // This keeps track of the number of lines read by the fstream object
+
+    x_prev = 0;                          // Used by NN filtering
+    y_prev = 0;                          // Used by NN filtering
+    p_prev = 0;                          // Used by NN filtering
+
+    glGenTextures(1, &texture_recon);
+    glBindTexture(GL_TEXTURE_2D, texture_recon);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+}
+
+guiProcess::~guiProcess() {
+
+}
+
+Events guiProcess::preprocess_file() {
+    if (!ref_period && !nn_window) {
+        Events process = read_file(filename, height, width);
+        return process;
+    } else {
+        Events input_event = read_file(filename, height, width);
+        Events process;
+    
+        if (ref_period && nn_window) {
+            Events ref_event;
+            refractoryFiltering(input_event, ref_event, ref_period);
+            ref_event.event_count = ref_event.x.size();
+
+            nnFiltering(ref_event, process, nn_window);
+            process.event_count = process.x.size();
+            ref_event.~Events();
+        } else if (ref_period) {
+            refractoryFiltering(input_event, process, ref_period);
+            process.event_count = process.x.size();
+        } else if (nn_window) {
+            refractoryFiltering(input_event, process, nn_window);
+            process.event_count = process.x.size();
+        }
+
+        return process;
+    }
+}
+
+void guiProcess::preprocess_initialisation() {
+    image = cv::Mat(height, width, CV_8UC1, cv::Scalar(0));
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // glTexParameteri(GLenum target, GLenum pname, GLfloat param):
+    // Sets the texture parameter.
+    // "target" Specifies the target to which the texture is bound for glTexParameter function.
+    // "pname" Specifies the symbolic name of a single-valued texture parameter
+    // "param" Specifies the value of pname
+    // GL_TEXTURE_MIN_FILTER:
+    // The texture minifying function is used whenever the level-of-detail function used when sampling from teh texture determines that the texture
+    // should be minified. Functions include: GL_NEAREST, GL_LINEAR, GL_NEAREST_MIPMAP_NEAREST, GL_LINEAR_MIPMAP_NEAREST, GL_NEAREST_MIPMAP_LINEAR, GL_LINEAR_MIPMAP_LINEAR
+    // GL_TEXTURE_MAG_FILTER:
+    // The texture magnification function is used whenever the level-of-detail function used when sampling from the texture determines that the texture should be magnified.
+    // The Functions include: GL_NEAREST, GL_LINEAR
+    // glPixelStorei:
+    // Sets pixel storage modes taht affect the operation of subsequent glReadPixels as well as the unpacking of texture patterns (like glTextImage2D & glTexSubImage2D)
+    // These settings stick with the texture that's bound. You only need to set them once.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+    // Allocate memory on the graphics card for the texture. It's fine if image is empty, the texture will just appear black until you update it.
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data);
+
+    ttos = 2*(2*ktos + 1);
+
+    if (tos_check) {
+        mode = "TOS";
+        surface_area = cv::Mat(height, width, CV_8UC1, cv::Scalar(0));
+        std::cout << "TOS" << std::endl;
+    } else {
+        mode = "timestamp";
+        surface_area = cv::Mat(height, width, CV_32SC1, cv::Scalar(0));
+        std::cout << "timestamp" << std::endl;
+    }
+    process_events = preprocess_file();
+    // std::cout << process_events.event_count << std::endl;
+}
+
+void guiProcess::preprocess_run() {
+    const int pol = process_events.p[event_no] ? 1:0;
+    int32_t x = process_events.x[event_no];
+    int32_t y = process_events.y[event_no];
+    int32_t t = process_events.t[event_no];
+
+    update_sae(surface_area, mode, x, y, t, time_window, quant, prev_time, ktos, ttos, width, height);
+
+    cv::cvtColor(surface_area, image, cv::COLOR_BGR2RGBA);
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.cols, image.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data);
+    ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(texture)), ImVec2(width, height));
+
+    prev_time = t; // Update prev_time
+    event_no++;
+
+    if (event_no >= process_events.event_count || (event_no >= no_of_events && no_of_events != -1)) {
+        preprocessing = true;
+        display_processed = false;
+    }
+}
+
+int guiProcess::run_display_reconstructed() {
+    image_recon = cv::imread(recon_path + std::to_string(frame) + ".jpg", cv::IMREAD_COLOR);
+    if (image_recon.empty()) return 0;
+
+    cv::cvtColor(image_recon, image_recon, cv::COLOR_BGR2RGBA);
+
+    if (first_recon) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_recon.cols, image_recon.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_recon.data);
+        first_recon = false;
+    }
+
+    ImGui::SetNextWindowSize(ImVec2(0,0));
+
+    glBindTexture(GL_TEXTURE_2D, texture_recon);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_recon.cols, image_recon.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_recon.data);
+    ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(texture_recon)), ImVec2(image_recon.cols, image_recon.rows));
+    
+
+    if (frame%1530 == 0) frame = 1;
+    else frame++;
+
+    frame_debug++; // This displays the total number of frames displayed
+    return 1;
+}
+
+void guiProcess::live_process_initialisation() {
+    if (!filename.empty()) event_file.open(filename, std::ios::in);
+
+    // end_event = (end_event == 0) ? lines : end_event; // Check the terminating condition. If end_event == 0, it means loop till the end of the file.
+
+    ref_mask = std::vector<std::vector<int>>(height, std::vector<int>(width, -ref_period));
+    nn_mask = std::vector<std::vector<int>>(height, std::vector<int>(width, -nn_window));
+
+    ttos = 2*(2*ktos + 1);
+
+    if (tos_check) {
+        mode = "TOS";
+        surface_area = cv::Mat(height, width, CV_8UC1, cv::Scalar(0));
+    } else {
+        mode = "timestamp";
+        surface_area = cv::Mat(height, width, CV_32SC1, cv::Scalar(0));
+    }
+}
+
+void guiProcess::live_process_run() {
+    std::string temp;
+    int32_t x = 0;
+    int32_t y = 0;
+    int32_t t = 0;
+    int p = 0;
+
+    if (!std::getline(event_file, temp)) {
+        display_live = false;
+        return;
+    }
+    std::stringstream ss(temp);
+    std::string s;
+    int count = 0;
+    while (ss.good()) {
+        std::getline(ss, s, ' ');
+        if (count == 0) {
+            double f = std::stod(s);
+            t = (int)(f*multiplier);
+        } else if (count == 1) {
+            x = std::stoi(s);
+        } else if (count == 2) {
+            y = std::stoi(s);
+        } else {
+            s.erase(std::remove(s.begin(), s.end(), '\n'), s.end());
+            if (s == "1") p = 1;
+            else p = 0;
+        }
+        count++;
+    }
+
+    // Check if the event passes Refractory Filtering
+    if (ref_period) {
+        if (!refractoryFiltering_live(ref_mask, x, y, t, p, ref_period)) {
+            line_no++;
+
+            cv::cvtColor(surface_area, image, cv::COLOR_BGR2RGBA);
+
+            glBindTexture(GL_TEXTURE_2D, texture);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.cols, image.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data);
+            ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(texture)), ImVec2(width, height));
+
+            return;
+        }
+    }
+    
+    // Check if the event passes NN Filtering
+    if (nn_window) {
+        if (!nnFiltering_live(nn_mask, x, y, t, p, x_prev, y_prev, p_prev, width, height, nn_window)) {
+            line_no++;
+
+            cv::cvtColor(surface_area, image, cv::COLOR_BGR2RGBA);
+
+            glBindTexture(GL_TEXTURE_2D, texture);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.cols, image.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data);
+            ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(texture)), ImVec2(width, height));
+
+            return;
+        }
+    }
+    
+    update_sae(surface_area, mode, x, y, t, time_window, quant, prev_time, ktos, ttos, width, height);
+
+    cv::cvtColor(surface_area, image, cv::COLOR_BGR2RGBA);
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.cols, image.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data);
+    ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(texture)), ImVec2(width, height));
+
+    prev_time = t; // Update prev_time
+    line_no++;
+}
+
+void guiProcess::resetFunction() {
+    on_off_check = false;
+    tos_check = false;
+    start_processing = false;
+    display_reconstructed = false;
+    display_live = false;
+    display_processed = false;
+    preprocessing = false;
+    reset = false;
+    davis240 = false;
+    davis346 = false;
+    first_recon = true;
+    first_live = true;
+
+    recon_path = "";
+    filename = "";
+
+    frame = 1;
+    frame_debug = 1;
+    time_window = 0;
+    height = 0;
+    width = 0;
+    ref_period = 0;
+    nn_window = 0;
+    no_of_events = -1;
+    event_no = 0;
+    ktos = 0;
+    ttos = 0;
+    prev_time = 0;
+    quant = 0;
+    //glDeleteTextures(1, &texture);
+
+    if (event_file.is_open()) event_file.close();
+}
